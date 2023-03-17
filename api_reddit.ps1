f5c6add7c563d2f382a06e2c57ceb7ba7da3f75e
Mahfuz <mahfuz.alam@cloverinfotech.com>
2023-03-17 18:41:01 +0530

    test

---
+++ api_reddit.ps1
@@ -0,0 +1,3 @@
+﻿$data = Invoke-RestMethod -Method Get -Uri 'http://reddit.com/.json'
+echo $data
+echo $data.data.children[0]
---
+++ for.ps1
@@ -0,0 +1,13 @@
+﻿# (# is used to comment a single line)
+
+<# This is multiline comment opening 
+ this is multiline comment closing#>
+  
+for($i=1;$i-le10;$i++)
+{
+    $x=$i%2
+    if($x -eq 0)
+    {
+        echo $i
+    }
+}
