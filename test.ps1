e65499ae94d5344879053ee5c1a0306e65115015
Mahfuz <mahfuz.alam@cloverinfotech.com>
2023-03-20 12:53:22 +0530

    api responce

---
+++ test.ps1
@@ -0,0 +1,20 @@
+﻿#$Username="mahfuz"
+#$Password="xyz"
+#$url="https://reqres.in/api/users"
+
+#$response=Invoke-RestMethod -Method Get -Uri 'https://reqres.in/api/users'
+#Write-Output "$data"
+
+$responce=Invoke-WebRequest -Uri https://reqres.in/api/users
+$responce.inputfields | Where-Object {
+    $_.id -like "*id"
+    $_.email -like "*email"
+    $_.first_name -like "*first_name"
+    $_.last_name -like "*last_name"
+    $_.avatar -like "*avatar"
+}
+
+Write-Host "$responce"
+
+
+
