9e23c4c588ac65d118f051ef6cd41336c9c2c122
Mahfuz <mahfuz.alam@cloverinfotech.com>
2023-03-21 12:37:43 +0530

    api_calling

---
+++ own_api.ps1
@@ -0,0 +1,28 @@
+﻿$response=Invoke-RestMethod -Uri https://641856d175be53f451dcbd82.mockapi.io/callingapicom/api/v1/login
+#$response | Format-Table | Out-String
+
+$response1=$response | Format-List | Out-String
+
+$hashtb=@{}
+$response | ForEach-Object{
+$hashtb.Add($_.username,$_.password)
+
+}
+
+$hashtb
+$hashtb | Out-File -FilePath D:\ps\response.txt
+
+
+<#
+$response1|foreach {
+    [PSCustomObject]@{
+        username = $_.username -as [string]
+        password = $_.password -as [string]
+        id       = $_.id -as [int32]
+    }
+
+}
+
+$response1 |  ConvertTo-Json | Out-File D:\ps\own_api.json
+#>
+
