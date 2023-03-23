ca77668a864978c21c1d38c609a0efa92ddbdef4
Mahfuz <mahfuz.alam@cloverinfotech.com>
2023-03-23 12:16:58 +0530

    match

---
+++ Matching_user_pass.ps1
@@ -0,0 +1,23 @@
+﻿$api=Invoke-RestMethod -Uri https://641856d175be53f451dcbd82.mockapi.io/callingapicom/api/v1/login
+
+$apiuser=$api.username
+Write-Host "$apiuser"
+Write-Host "************"
+
+$apipass=$api.password
+Write-Output "$apipass"
+Write-Output "-----------"
+
+<#$apiid=$api.id
+Write-Host "$apiid"
+Write-Host "+++++++++++++"
+#>
+if($apiuser -eq "godwar" -and $apipass -eq "war")
+{
+    Write-Output "Inside if loop"
+}
+else
+{
+    Write-Output "Else loop"
+    throw 'Auth fail'
+}
